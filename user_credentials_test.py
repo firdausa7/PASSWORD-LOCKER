58764ea0fa537d677d0091f8f92e2e51d69e7742
firdausa7 <firdausa.salat@gmail.com>
2019-09-04 08:45:30 +0300

    credential test successful

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -8,3 +8,22 @@ class TestInfo(unittest.TestCase):
         self.assertEqual(self.new_info.twitter_t,"firdausa.salat")
         self.assertEqual(self.new_info.email_e,"salat.firdausa")
     def test_save_info(self):
+        '''
+        to test if user info is saved
+        '''
+        self.new_info.save_info()
+        self.assertEqual(len(Info.info_list),1)
+    def tearDown(self):
+        Info.info_list = []
+    def test_delete_info(self):
+        self.new_info.save_info()
+        test_info = Info("hanna.23","hanna.23")
+        test_info.save_info()
+        test_info.delete_info()
+        self.assertEqual(len(Info.info_list),1)
+    def test_display_creds(self):
+        self.assertEqual(Info.display_info(),Info.info_list)
+
+if __name__ == '__main__':
+    unittest.main()
+
